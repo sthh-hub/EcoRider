0eaf14fd67888f5b3e64e91b92e09ec6aa5b0469
bodhiforge <happeryang2046@gmail.com>
2024-09-21 15:55:34 -0700

    try a deployment

--- server/main.go
+++ server/main.go
@@ -14,9 +14,9 @@ func main() {
     // Register the handler function with the path "/data"
     http.HandleFunc("/data", getData)
     
-    // Start the HTTP server on port 8080
-    fmt.Println("Server starting on port 8080...")
-    err := http.ListenAndServe(":8080", nil)
+    // Start the HTTP server on port 80
+    fmt.Println("Server starting on port 80...")
+    err := http.ListenAndServe(":80", nil)
     if err != nil {
         fmt.Println("Error starting server:", err)
     }
