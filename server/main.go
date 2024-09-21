c0439f3301ca3517b7698b6862c6ad647f324c54
bodhiforge <happeryang2046@gmail.com>
2024-09-21 16:04:48 -0700

    HTTPS

--- server/main.go
+++ server/main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-// Handler function for the "/data" path
 func getData(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Hello world\n") // Writing response to the client
+	fmt.Fprintf(w, "Hello, Secure World!\n")
 }
 
 func main() {
-    // Register the handler function with the path "/data"
-    http.HandleFunc("/data", getData)
-    
-    // Start the HTTP server on port 80
-    fmt.Println("Server starting on port 80...")
-    err := http.ListenAndServe(":80", nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	// Handler for HTTPS requests
+	http.HandleFunc("/data", getData)
+
+	// Setting up a TLS configuration with supported protocols
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// Starting the HTTPS server
+	server := &http.Server{
+		Addr:      ":443",
+		TLSConfig: tlsConfig,
+	}
+
+	// Redirect HTTP to HTTPS
+	go func() {
+		log.Println("Starting HTTP to HTTPS redirect server on :80")
+		err := http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+		}))
+		if err != nil {
+			log.Fatalf("HTTP redirect server failed: %v", err)
+		}
+	}()
+
+	log.Println("Starting HTTPS server on :443")
+	err := server.ListenAndServeTLS(
+		"/etc/letsencrypt/live/api.sheribo.site/fullchain.pem",
+		"/etc/letsencrypt/live/api.sheribo.site/privkey.pem",
+	)
+	if err != nil {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
+	}
 }
